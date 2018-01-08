2d8d316ace73479929a96f474f3b11fb750e8cd8
François Rosato <francois.rosato@ekino.com>
2018-01-08 18:16:02 +0100

    [-] remove imports

--- src/imports.js
+++ src/imports.js
@@ -1,6 +1,1 @@
-import '/Users/rosato/Desktop/Apps/test/sketch-project/react-sketch-book/example/src/components/molecules/TextBlockActions/index.sketch.js'
-import '/Users/rosato/Desktop/Apps/test/sketch-project/react-sketch-book/example/src/components/atoms/Title/index.sketch.js'
-import '/Users/rosato/Desktop/Apps/test/sketch-project/react-sketch-book/example/src/components/atoms/Button/index.sketch.js'
-import '/Users/rosato/Desktop/Apps/test/sketch-project/react-sketch-book/example/src/components/organisms/Hero/index.sketch.js'
-import '/Users/rosato/Desktop/Apps/test/sketch-project/react-sketch-book/example/src/components/organisms/News/index.sketch.js'
 export default {}
